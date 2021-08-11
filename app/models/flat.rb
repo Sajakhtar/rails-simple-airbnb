6d457ba62c5d693625d20ca63b9577d6a61c0316
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-11 07:49:01 +0400

    migration for image_url attribute, update seed with image_url, update index view to add image_url in card, update show view to add image banner

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,4 +4,5 @@ class Flat < ApplicationRecord
   validates :description, presence: true
   validates :price_per_night, presence: true, numericality: true
   validates :number_of_guests, presence: true, numericality: true
+  validates :image_url, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ flat1 = {
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  image_url: 'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3300&q=80'
 }
 
 flat2 = {
@@ -23,7 +24,8 @@ flat2 = {
   address: 'Address JBR, Dubai Marina, Dubai',
   description: 'A lovely holiday feel with breathtaking views of the ocean. One double bedrooms, open plan living area, large kitchen and jacuzzi',
   price_per_night: 150,
-  number_of_guests: 2
+  number_of_guests: 2,
+  image_url: 'https://images.unsplash.com/photo-1523816572-a1a23d1a67b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3302&q=80'
 }
 
 flat3 = {
@@ -31,7 +33,8 @@ flat3 = {
   address: 'Address Downtown, DIFC, Dubai',
   description: 'A business chic apartment with views of the burj. Two double bedrooms, open plan living area, small kitchen',
   price_per_night: 100,
-  number_of_guests: 4
+  number_of_guests: 4,
+  image_url: 'https://images.unsplash.com/photo-1580674684081-7617fbf3d745?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3367&q=80'
 }
 
 flat4 = {
@@ -39,7 +42,8 @@ flat4 = {
   address: 'Wall St, New York City, New York',
   description: 'A business chic apartment with views OWTC. On double bedrooms, open plan living area, kitchen',
   price_per_night: 125,
-  number_of_guests: 2
+  number_of_guests: 2,
+  image_url: ''
 }
 
 [flat1, flat2, flat3, flat4].each do |attributes|
